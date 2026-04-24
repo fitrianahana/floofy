0bdf8fc504626338f29ce42824db49866d0ee3df
JulianAsnaw975 <julianasna975@gmail.com>
2026-04-24 23:11:29 +0700

    Create ProductList view

---
+++ Views/ProductList/ProductList.xaml.cs
@@ -0,0 +1,31 @@
+using floofy.ViewModels;
+using System.Windows.Input;
+namespace floofy.Views;
+
+public partial class ProductList : ContentPage
+{
+    private readonly ProductListViewModel _viewModel;
+    public ProductList(ProductListViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = viewModel;
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        // Load products when page appears
+        if (_viewModel.LoadProductsCommand is ICommand cmd && cmd.CanExecute(null))
+        {
+            cmd.Execute(null);
+        }
+    }
+    private async void OnSearchButtonPressed(object sender, EventArgs e)
+    {
+        // Execute search command when search button is pressed
+        if (_viewModel.SearchCommand is ICommand cmd && cmd.CanExecute(null))
+        {
+            cmd.Execute(null);
+        }
+    }
+}
